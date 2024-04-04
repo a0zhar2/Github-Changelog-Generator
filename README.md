6e2699f3513f37870b44ecd6ccc9afd29553cd90
a0zhar2 <166025046+a0zhar2@users.noreply.github.com>
2024-04-04 15:01:52 +0200

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
-# Github-Changelog-Generator
-A fully web-based, easy to use, github repository changelog generator... Which generates markdown based changelog using the commits made to the repository
+# Github Changelog Generator
+Easy to use, Fully Web-based, Github Markdown style Changelog Generator. All it needs is the Account Username, and the Repository name, and it will generate changelog for that repository looking like:
+```md
+### Patches made on <YYYY-MM-DD>
+- <Commit Title>
+  - <Additional Data>
+  - <Additional Data>
+  - <Additional Data>
+  - <Additional Data>
+- <Commit Title>
+- <Commit Title>
+- <Commit Title>
+```
+
+It uses latest version of Bootstrap for the UI part, Javascript for the web-Request to get commits...
+
+----
+### Screenshots
+
+**Initial Form**
+<img width="300" src="images/UI.png" >
+
+**Generated Changelog**
+<img width="300" src="images/Generated.png" >
+
+<br>
+----
+
+### Credits
+- Me (A0ZHAR)
---
+++ generator.js
@@ -0,0 +1,81 @@
+// Helper function that will generate a changelog entry for a single commit:
+function generate_markdown_changelog(commitData) {
+  let built_markdown = "";
+  
+  // If the commit passed only has title, and no additional data,
+  // then we print the title only, and return early
+  if (commitData.length == 1) {
+    return `- ${commitData[0]}<br>`;
+  }
+  
+  // If the commit contains additional changes
+  if (commitData.length > 1) {
+    // Then we print them as a list
+    commitData.slice(1).forEach((change) => {
+      if (change.trim() !== "") {
+        // Prevent duplicate lines from being added
+        if (!built_markdown.includes(change))
+          built_markdown += `&nbsp;&nbsp;${change}<br>`;
+      }
+    });
+  }
+
+  return built_markdown;
+}
+
+document.getElementById("userinfo").addEventListener("click", function () {
+  const username   = document.getElementById("username").value;
+  const repository = document.getElementById("repoName").value;
+  const main_container = document.getElementById("mainform");
+  
+  // Check if either one of the input boxes contain no value,
+  // and let user know that they should input data, then we
+  // return early
+  if (username == "" || repository == "") {
+    let bak = main_container.innerHTML;
+    main_container.innerHTML = '<div class="alert alert-warning">One of the Input boxes are empty</div><br>'+bak;
+    return;
+  }
+
+  async function fetchCommits() {
+    const response = await fetch(`https://api.github.com/repos/${username}/${repository}/commits`);
+    return await response.json();
+  }
+
+  async function displayCommits() {
+    const commitsList = document.getElementById("commitsList");
+    let current_commit_date = null; // Variable to track the current date of commits
+    let changelog = "";             // Variable to store the entire changelog
+
+    try {
+      const commits = await fetchCommits();
+
+      commits.forEach((commit) => {
+        // Get the date of the current commit
+        const commitDate = commit.commit.author.date.toString().slice(0, 10);
+
+        // If the date of the current commit is different from the previous one,
+        // start a new "Changes on <date>" section
+        if (commitDate !== current_commit_date) {
+          if (current_commit_date !== null) {
+            // Close the previous "Changes on <date>" section
+            changelog += "<br>";
+          }
+          // Start a new "Changes on <date>" section
+          changelog += `### Patches made on (${commitDate})<br><br>`;
+          current_commit_date = commitDate;
+        }
+
+        // Generate the changelog entry for the current commit and add it to the changelog
+        changelog += generate_markdown_changelog(commit.commit.message.split("\n"));
+      });
+
+      // Add the completed changelog to the commitsList element
+      commitsList.innerHTML += changelog;
+    } catch (error) {
+      commitsList.innerHTML = '<div class="alert alert-danger">Error fetching commits. Please try again later.</div><br>'+bak;
+    }
+  }
+
+  displayCommits();
+});
